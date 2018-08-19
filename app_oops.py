c7ab6854923709ae4262b29ba427032333d31ae3
sambapython <sambapython@gmail.com>
2018-08-19 08:55:39 +0530

    2018-08-19 opps polymosrphism class, static, instance methods, pep8, pylint.

--- app_oops.py
+++ app_oops.py
@@ -1,7 +1,23 @@
+"""
+to explain oops concepts
+"""
+
 from m1 import emp
-class emp_extra(emp):
-	def sal_cal(self):
-		print "this is sal_cal"
-o1=emp_extra()
+
+
+class EmpExtra(emp):
+    """
+    to maintain emp data
+    """
+    def __init__(self):
+        pass
+    @staticmethod
+    def sal_cal(emp_name):
+        """
+        to calculate sal.
+        """
+        print "this is sal_cal"
+        print emp_name
+o1 = EmpExtra()
 o1.leavecal()
-o1.sal_cal()
+o1.sal_cal(10)
